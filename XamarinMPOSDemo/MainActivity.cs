8701be70fa9984a5e335da463703c3f1e8ada4a0
Thorsten Kramer <thorsten.kramer@payworksmobile.com>
2014-11-06 11:34:01 +0100

    Initial import
    uses a hacked version of the mPOS SDK 2.2 release

---
+++ XamarinMPOSDemo/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using IO.Mpos;
+
+namespace XamarinMPOSDemo
+{
+	[Activity (Label = "XamarinMPOSDemo", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+				StartPayment();
+			};
+		}
+
+
+		protected void StartPayment()
+		{
+			MposExtended.SetLogLevel (2); //TRACE
+
+			var provider = Mpos.CreateTransactionProvider (this, IO.Mpos.Provider.ProviderMode.Test, "", "");
+			var template = provider.CreateChargeTransactionTemplate (Java.Math.BigDecimal.One, IO.Mpos.Transactions.Currency.Eur, "subject", "identifier");
+			var listener = new MyPaymentProcessListener (FindViewById<TextView> (Resource.Id.textView1));
+			provider.StartPayment (template, IO.Mpos.Accessories.AccessoryFamily.MiuraMpi, listener);
+
+
+		}
+	}
+}
+
+
---
+++ XamarinMPOSDemo/MyPaymentProcessListener.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace XamarinMPOSDemo
+{
+	public class MyPaymentProcessListener : Java.Lang.Object, IO.Mpos.Transactionprovider.IPaymentProcessWithRegistrationListener
+	{
+		private TextView _statusView;
+
+		public MyPaymentProcessListener(TextView statusView) {
+			_statusView = statusView;
+		}
+
+		public void OnRegistered (IO.Mpos.Transactionprovider.IPaymentProcess paymentProcess, IO.Mpos.Transactions.ITransaction transaction)
+		{
+			updateStatus("OnRegistered");
+		}
+
+		#region IPaymentProcessListener implementation
+
+		public void OnApplicationSelectionRequired (IO.Mpos.Transactionprovider.IPaymentProcess p0, IO.Mpos.Transactions.ITransaction p1, System.Collections.Generic.IList<IO.Mpos.Paymentdetails.IApplicationInformation> p2)
+		{
+			updateStatus("application selection required.");
+			p0.ContinueWithSelectedApplication (p2 [0]);
+		}
+
+		public void OnCompleted (IO.Mpos.Transactionprovider.IPaymentProcess paymentProcess, IO.Mpos.Transactions.ITransaction transaction, IO.Mpos.Transactionprovider.IPaymentProcessDetails details)
+		{
+			updateStatus ("OnCompleted");
+		}
+
+		public void OnCustomerSignatureRequired (IO.Mpos.Transactionprovider.IPaymentProcess paymentProcess, IO.Mpos.Transactions.ITransaction transaction)
+		{
+			updateStatus ("OnCustomerSignatureRequired");
+			paymentProcess.ContinueWithCustomerSignatureOnReceipt (true);
+		}
+
+		public void OnCustomerVerificationRequired (IO.Mpos.Transactionprovider.IPaymentProcess paymentProcess, IO.Mpos.Transactions.ITransaction transaction)
+		{
+			updateStatus ("OnCustomerVerificationRequired");
+			paymentProcess.ContinueWithCustomerIdentityVerified (true);
+		}
+
+		public void OnStatusChanged (IO.Mpos.Transactionprovider.IPaymentProcess paymentProcess, IO.Mpos.Transactions.ITransaction transaction, IO.Mpos.Transactionprovider.IPaymentProcessDetails details)
+		{
+			updateStatus ("onStatusChanged:\n" + details.GetInformation()[0] + "\n" + details.GetInformation()[1]);
+		}
+
+		#endregion
+
+		private void updateStatus(String text) {
+			Log.Info ("MyPaymentProcessListener", text);
+			_statusView.Text = text;
+		}
+	}
+}
+
